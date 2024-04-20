62e0920b4891e8e26867826d74633fe82c3b7c07
Mayank <agarwalm92@gmail.com>
2024-04-21 01:15:03 +0530

    Making mobile nav

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,5 +1,66 @@
+import { useState } from "react";
+import { AiOutlineHome, AiOutlineMenu } from "react-icons/ai";
+import { GrProjects } from "react-icons/gr";
+import { MdOutlineWorkOutline } from "react-icons/md";
+import { BsPerson } from "react-icons/bs";
+import { MdOutlineMailOutline } from "react-icons/md";
+
 const Sidebar = () => {
-  return <div>This is a Sidebar</div>;
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+    console.log("state");
+  };
+
+  return (
+    <div>
+      <AiOutlineMenu
+        onClick={handleNav}
+        className="absolute top-4 right-4 z-[99] md:hidden"
+      />
+      {nav ? (
+        <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <AiOutlineHome size={20} />
+            <span className="pl-4">Home</span>
+          </a>
+          <a
+            href="#work"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <MdOutlineWorkOutline size={20} />
+            <span className="pl-4">Work</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <GrProjects size={20} />
+            <span className="pl-4">Project</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <BsPerson size={20} />
+            <span className="pl-4">Resume</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <MdOutlineMailOutline size={20} />
+            <span className="pl-4">Contact</span>
+          </a>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 };
 
 export default Sidebar;
