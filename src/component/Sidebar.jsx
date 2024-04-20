f9d04e46cc61cc80f6b38e24d79a9c8379fbe660
Mayank <agarwalm92@gmail.com>
2024-04-21 01:41:48 +0530

    Making Sidebar with media conditon

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -28,28 +28,28 @@ const Sidebar = () => {
             <span className="pl-4">Home</span>
           </a>
           <a
-            href="#work"
+            href="#Work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <MdOutlineWorkOutline size={20} />
             <span className="pl-4">Work</span>
           </a>
           <a
-            href="#main"
+            href="#Project"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <GrProjects size={20} />
             <span className="pl-4">Project</span>
           </a>
           <a
-            href="#main"
+            href="#Resume"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <BsPerson size={20} />
             <span className="pl-4">Resume</span>
           </a>
           <a
-            href="#main"
+            href="#Contact"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <MdOutlineMailOutline size={20} />
@@ -59,6 +59,41 @@ const Sidebar = () => {
       ) : (
         ""
       )}
+
+      <div className="top-[25%] fixed z-10 md:block hidden">
+        <div className="flex flex-col">
+          <a
+            href="#main"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <AiOutlineHome size={20} />
+          </a>
+          <a
+            href="#Work"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <MdOutlineWorkOutline size={20} />
+          </a>
+          <a
+            href="#Project"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <GrProjects size={20} />
+          </a>
+          <a
+            href="#Resume"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <BsPerson size={20} />
+          </a>
+          <a
+            href="#Contact"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <MdOutlineMailOutline size={20} />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
