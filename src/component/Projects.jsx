69dbc2aaf2cc66dc509cfb1d185b3aa7ab6c9276
Mayank <agarwalm92@gmail.com>
2024-04-21 17:58:14 +0530

    added project items, added margin in headers

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,7 +1,16 @@
+import { projects } from "../data/workData";
+import ProjectItem from "./ProjectItem";
+import gamehub from "../assets/gamehub.jpg";
+
 const Projects = () => {
   return (
     <div id="project" className="section">
       <h1 className="header">Project</h1>
+      <div className="grid sm:grid-cols-2 gap-12">
+        {projects.map((p) => (
+          <ProjectItem key={p.id} title={p.title} img={gamehub} />
+        ))}
+      </div>
     </div>
   );
 };
